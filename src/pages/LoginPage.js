82cfb6867d6ac6d8e54959552655dc0ba9bb9eab
jellysoo97 <jellysoo97@gmail.com>
2022-07-31 11:20:37 +0900

    fix: post login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,15 +2,30 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Login from '../components/Login/Login';
-import { useRecoilValue } from 'recoil';
-import { loginState } from './../atom';
+import { useRecoilValue, useRecoilState } from 'recoil';
+import { checkLoginState, loginState } from './../atom';
 
 function LoginPage() {
   const login = useRecoilValue(loginState);
+  const [checked, setChecked] = useRecoilState(checkLoginState);
 
-  console.log(login);
+  useEffect(() => {
+    async function postLoginData() {
+      try {
+        const data = await axios.post('url', login).then(res => {
+          console.log(res);
+          setChecked(!checked);
+          setLoading(false);
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    setLoading(true);
+    postLoginData();
+  }, []);
 
-  return <Login />;
+  return <>{loading ? <Loading /> : <Login />}</>;
 }
 
 export default LoginPage;
